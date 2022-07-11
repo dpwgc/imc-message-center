4c5166929cff14a4e4380336a8a308bd748106a9
dpwgc <281148314@qq.com>
2022-07-11 16:18:34 +0800

    引入mapstruct装配器

--- message-center-infrastructure/src/main/java/com/dpwgc/message/center/infrastructure/assembler/InformationPOAssembler.java
+++ message-center-infrastructure/src/main/java/com/dpwgc/message/center/infrastructure/assembler/InformationPOAssembler.java
@@ -1,4 +1,11 @@
 package com.dpwgc.message.center.infrastructure.assembler;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
 public interface InformationPOAssembler {
+
+    InformationPOAssembler INSTANCE = Mappers.getMapper(InformationPOAssembler.class);
+
 }
--- message-center-infrastructure/src/main/java/com/dpwgc/message/center/infrastructure/assembler/MessagePOAssembler.java
+++ message-center-infrastructure/src/main/java/com/dpwgc/message/center/infrastructure/assembler/MessagePOAssembler.java
@@ -1,4 +1,14 @@
 package com.dpwgc.message.center.infrastructure.assembler;
 
+import com.dpwgc.message.center.domain.chat.message.Message;
+import com.dpwgc.message.center.infrastructure.dal.chat.entity.MessagePO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
 public interface MessagePOAssembler {
+
+    MessagePOAssembler INSTANCE = Mappers.getMapper(MessagePOAssembler.class);
+
+    MessagePO assemblerMessagePO(Message message);
 }
