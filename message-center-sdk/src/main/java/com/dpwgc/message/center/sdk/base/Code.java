5ce5f8c053b517068580dbd6d777977464e5d329
dpwgc <281148314@qq.com>
2022-07-11 15:24:18 +0800

    新增http请求返回数据封装

---
+++ message-center-sdk/src/main/java/com/dpwgc/message/center/sdk/base/Code.java
@@ -0,0 +1,6 @@
+package com.dpwgc.message.center.sdk.base;
+
+public interface Code {
+    Integer SUCCESS = 0;
+    Integer ERROR = -1;
+}
---
+++ message-center-sdk/src/main/java/com/dpwgc/message/center/sdk/base/ResultDTO.java
@@ -0,0 +1,67 @@
+package com.dpwgc.message.center.sdk.base;
+
+public class ResultDTO<T> {
+    protected boolean success;
+    protected Integer code;
+    protected String message;
+    protected T data;
+
+    public Integer getCode() {
+        return this.code;
+    }
+
+    public ResultDTO<T> setCode(Integer code) {
+        this.code = code;
+        return this;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public ResultDTO<T> setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public ResultDTO() {
+    }
+
+    public boolean isSuccess() {
+        return this.success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public T getData() {
+        return this.data;
+    }
+
+    public ResultDTO<T> setData(T data) {
+        this.data = data;
+        return this;
+    }
+
+    public static <T> ResultDTO<T> getSuccessResult(T v) {
+        ResultDTO<T> result = new ResultDTO();
+        result.setSuccess(true);
+        result.setCode(Code.SUCCESS);
+        result.setMessage("success");
+        result.setData(v);
+        return result;
+    }
+
+    public static <T> ResultDTO<T> getFailureResult(String msg) {
+        ResultDTO<T> result = new ResultDTO();
+        result.setSuccess(false);
+        result.setCode(Code.ERROR);
+        result.setMessage(msg);
+        return result;
+    }
+
+    public String toString() {
+        return "ResultDTO(success=" + this.isSuccess() + ", code=" + this.getCode() + ", message=" + this.getMessage() + ", data=" + this.getData() + ")";
+    }
+}
