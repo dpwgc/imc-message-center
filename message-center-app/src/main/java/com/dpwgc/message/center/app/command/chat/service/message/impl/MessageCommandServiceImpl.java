331b1a10b22fa8d86ea3efe7d877fcb22e6955a1
dpwgc <281148314@qq.com>
2022-07-13 20:36:33 +0800

    优化消息撤回功能

--- message-center-app/src/main/java/com/dpwgc/message/center/app/command/chat/service/message/impl/MessageCommandServiceImpl.java
+++ message-center-app/src/main/java/com/dpwgc/message/center/app/command/chat/service/message/impl/MessageCommandServiceImpl.java
@@ -9,6 +9,7 @@ import com.dpwgc.message.center.infrastructure.util.BroadcastUtil;
 import com.dpwgc.message.center.infrastructure.util.IdGenUtil;
 import com.dpwgc.message.center.sdk.model.chat.message.CreateMessageCommand;
 import com.dpwgc.message.center.sdk.model.chat.message.CreateMessageWsCommand;
+import com.dpwgc.message.center.sdk.model.chat.message.MessageDTO;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -63,8 +64,11 @@ public class MessageCommandServiceImpl implements MessageCommandService {
 
         if (message != null) {
             if (message.getStatus() == 0) {
+                //去除消息主体内容
+                MessageDTO messageDTO = messageAssembler.assembleMessageDTO(message);
+                messageDTO.setContent("");
                 //广播消息
-                return broadcastUtil.broadcast(messageAssembler.assembleMessageDTO(message));
+                return broadcastUtil.broadcast(messageDTO);
             }
         }
 
