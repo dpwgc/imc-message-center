f92b374dddc8813281fb327adbbdf16186a71d17
dpwgc <281148314@qq.com>
2022-07-18 12:26:46 +0800

    新增组件扫描

--- message-center-ui/src/main/java/com/dpwgc/message/center/ui/MessageCenterApplication.java
+++ message-center-ui/src/main/java/com/dpwgc/message/center/ui/MessageCenterApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.dpwgc.message.center.app")
 @ComponentScan("com.dpwgc.message.center.ui.config")
 @ComponentScan("com.dpwgc.message.center.infrastructure.util")
+@ComponentScan("com.dpwgc.message.center.infrastructure.component")
 @ComponentScan("com.dpwgc.message.center.infrastructure.repository")
 @MapperScan("com.dpwgc.message.center.infrastructure.dal")
 @SpringBootApplication
